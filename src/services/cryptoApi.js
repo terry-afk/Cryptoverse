9d4fb8a9e317dd1d0da9e1e31a865b8e9746b384
Alexis Fabarez <alexis.fabarez@epitech.eu>
2022-02-06 15:29:17 +0100

    [ADD] redux store and first api call

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'a3d1b80521mshee111d369c90db6p122a53jsn5ca460086ec2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/exchanges')
+    })
+  })
+})
