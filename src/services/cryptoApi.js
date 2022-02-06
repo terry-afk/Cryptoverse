c2484fe4f08a28a9d6014bbc92a8ba9fd4b07612
Alexis Fabarez <alexis.fabarez@epitech.eu>
2022-02-06 16:09:45 +0100

    [ADD] display of fetched data

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
   'x-rapidapi-key': 'a3d1b80521mshee111d369c90db6p122a53jsn5ca460086ec2'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/exchanges')
+      query: () => createRequest('/coins')
     })
   })
 })
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi
